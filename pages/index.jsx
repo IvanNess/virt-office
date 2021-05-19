b86ace6b2f9d520120b0ac5520ec6403ec495803
IvanNess <chai@bk.ru>
2021-05-19 10:48:45 +0200

    carousel autoplay true

--- pages/index.jsx
+++ pages/index.jsx
@@ -42,7 +42,7 @@ export default function Home({db, auth}) {
           <input className={styles.textInput} type="text" name="" id="" placeholder="Wpisz miasto"/>
           <input className={styles.buttonInput} type="button" value="WYSZUKAJ"/>
         </form> */}
-        <Carousel autoplay={false} autoplaySpeed={4000} pauseOnHover={false}>
+        <Carousel autoplay={true} autoplaySpeed={4000} pauseOnHover={false}>
           <div className={styles.headerOne}>
             <div className={styles.headerFlex}>
               <div className={styles.headerTitle}>
