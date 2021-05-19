6f89c65dae045ed1b1088ec7e2cb06cdb5f42f12
IvanNess <chai@bk.ru>
2021-05-19 10:28:55 +0200

    useWindowWidth

--- hooks/useWindowWidth.js
+++ hooks/useWindowWidth.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 const useWindowWidth = () => {
 
   // Store the state
-  const [width, setWidth] = useState(document.documentElement.clientWidth);
+  const [width, setWidth] = useState(undefined);
 
   // On Scroll
   const onChange = () => {
@@ -13,11 +13,13 @@ const useWindowWidth = () => {
 
   // Add and remove the window listener
   useEffect(() => {
+    console.log('RESIZE')
+    setWidth(document.documentElement.clientWidth);
     window.addEventListener("resize", onChange);
     return () => {
       window.removeEventListener("resize", onChange);
     };
-  });
+  }, [])
 
   if (typeof window === "undefined") return 500;
 
